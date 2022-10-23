ec39b7bf8e48a7368c9ecf67e9d19a54218d046d
TheEntropyShard <theentropyshard@gmail.com>
2022-10-23 15:45:07 +0300

    Added mail boxes ids, next sending mail

---
+++ src/main/java/me/theentropyshard/netschoolapi/mail/schemas/MailBoxIds.java
@@ -0,0 +1,10 @@
+package me.theentropyshard.netschoolapi.mail.schemas;
+
+public final class MailBoxIds {
+    private MailBoxIds() {}
+
+    public static final int BOX_INCOMING = 1;
+    public static final int BOX_DRAFTS = 2;
+    public static final int BOX_SENT = 3;
+    public static final int BOX_DELETED = 4;
+}
